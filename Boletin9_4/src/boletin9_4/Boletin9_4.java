497d54553087bac7619e9654a91bc6d3a674a2f1
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-11-14 09:19:51 +0100

    clase main

---
+++ Boletin9_4/src/boletin9_4/Boletin9_4.java
@@ -0,0 +1,13 @@
+package boletin9_4;
+/**
+ * @author Juan Borrajo Rodriguez Nº5937
+ */
+public class Boletin9_4 {
+
+    public static void main(String[] args) {
+        Multi num=new Multi();
+        num.tabla();
+
+    }
+
+}
