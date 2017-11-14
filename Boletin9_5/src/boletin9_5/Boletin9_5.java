1da6352625a37edd04f435b9529a946ec13cae4e
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-11-14 09:45:20 +0100

    Clase main

---
+++ Boletin9_5/src/boletin9_5/Boletin9_5.java
@@ -0,0 +1,13 @@
+package boletin9_5;
+/**
+ * @author Juan Borrajo Rodriguez Nº5937
+ */
+public class Boletin9_5 {
+
+    public static void main(String[] args) {
+        Bucle secuencia=new Bucle();
+        secuencia.serie();
+
+    }
+
+}
