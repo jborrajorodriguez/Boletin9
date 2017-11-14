80316a0ac6fb5d6f58b8a3ac634b52e72bce6506
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-11-14 09:02:20 +0100

    Clase main

---
+++ Boletin9_3/src/boletin9_3/Boletin9_3.java
@@ -0,0 +1,12 @@
+package boletin9_3;
+/**
+ * @author Juan Borrajo Rodriguez Nº5937
+ */
+public class Boletin9_3 {
+
+    public static void main(String[] args) {
+        Area calculo=new Area();
+        calculo.calcular();
+    }
+
+}
