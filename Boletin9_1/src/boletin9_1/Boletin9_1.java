567b2607a18b27611310b0e13fb5d7e1ca6b573e
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-11-20 08:54:54 +0100

    Clase main

---
+++ Boletin9_1/src/boletin9_1/Boletin9_1.java
@@ -0,0 +1,13 @@
+package boletin9_1;
+/**
+ * @author Juan Borrajo Rodriguez Nº5937
+ */
+public class Boletin9_1 {
+
+    public static void main(String[] args) {
+        Numeros a=new Numeros();
+        a.Separar();
+
+    }
+
+}
