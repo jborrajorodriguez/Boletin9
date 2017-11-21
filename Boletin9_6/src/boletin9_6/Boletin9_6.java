db3501a14d82ebfdd97859ab392aca52d23ef8e5
Juan <Juan@Juan-PC>
2017-11-21 15:22:10 +0100

    Clase main

---
+++ Boletin9_6/src/boletin9_6/Boletin9_6.java
@@ -0,0 +1,14 @@
+package boletin9_6;
+
+/**
+ *
+ * @author Juan
+ */
+public class Boletin9_6 {
+
+    public static void main(String[] args) {
+        Soldo a=new Soldo ();
+        a.traballadores();
+    }
+    
+}
